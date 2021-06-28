f3cd7247d361246fd818e75b657a0b4c55aed90a
unknown <jo7hb@naver.com>
2021-06-28 16:09:19 +0900

    Crawling

--- Crawling/CrawlingDriver.py
+++ Crawling/CrawlingDriver.py
@@ -9,14 +9,19 @@ class Crawling:
     def __init__(self) -> None:
         self.driver = webdriver.Edge(Edge().install())
         self.time: int = 100
+        self.category: list = ["politics", "economy", "inter", "society", "sports"]
 
     def crawlingTimer(self) -> None:
         while True:
+            # 비동기? 동기?
             # time 마다 한 사이트씩 크롤링을 진행
             # time 은 실제 동작 시간을 측정후 적절한 값을 선택해야할듯
             time.sleep(self.time)
             # Call
+            # DB insert
             time.sleep(self.time)
             # Call
+            # DB insert
             time.sleep(self.time)
             # Call
+            # DB insert
--- Crawling/DongaDriver.py
+++ Crawling/DongaDriver.py
@@ -6,7 +6,7 @@ class Donga(Crawling):
     def __init__(self) -> None:
         Crawling.__init__(self)
         # 각 카테고리별 링크
-        self.catecory: dict = {
+        self.catecoryDir: dict = {
             "politics": "https://www.donga.com/news/Politics",
             "economy": "https://www.donga.com/news/Economy",
             "inter": "https://www.donga.com/news/Inter",
@@ -15,70 +15,76 @@ class Donga(Crawling):
         }
 
     def startCrawling(self) -> None:
-        articleClass: selenium
         articlePhoto: selenium
         relationArticle: str
         articleText: str
         photoDes: list = []
         title: str
+
         try:
-            self.getSite("politics")
+            for category in self.category:
+                # 사이트 접근
+                self.getSite(category)
 
-            # 기사 제목
-            try:
-                title: str = self.driver.find_element_by_xpath(
-                    '//*[@id="container"]/div[2]/h1'
-                ).text
-            except:
-                title = ""
+                # 기사 제목
+                try:
+                    title: str = self.driver.find_element_by_xpath(
+                        '//*[@id="container"]/div[2]/h1'
+                    ).text
+                except:
+                    title = ""
 
-            # 기사 내용
-            try:
-                articleText = self.driver.find_element_by_xpath(
-                    '//*[@id="content"]/div/div[1]'
-                ).text
-            except:
-                articleText = ""
+                # 기사 내용
+                try:
+                    articleText = self.driver.find_element_by_xpath(
+                        '//*[@id="content"]/div/div[1]'
+                    ).text
+                except:
+                    articleText = ""
 
-            # 사진이 있을때
-            try:
-                articlePhoto = self.driver.find_elements_by_class_name("articlePhotoC")
-                for i in articlePhoto:
-                    photoDes.append(i.text)
+                # 사진이 있을때
+                try:
+                    articlePhoto = self.driver.find_elements_by_class_name(
+                        "articlePhotoC"
+                    )
+                    for i in articlePhoto:
+                        photoDes.append(i.text)
 
-                # 사진 description 지우기
-                for des in photoDes:
-                    articleText = articleText.replace(des, "")
-            except:
-                articlePhoto = None
+                    # 사진 description 지우기
+                    for des in photoDes:
+                        articleText = articleText.replace(des, "")
+                except:
+                    articlePhoto = None
 
-            # 관련 기사 지우기
-            try:
-                relationArticle = self.driver.find_element_by_class_name(
-                    "article_relation"
-                ).text
+                # 관련 기사 지우기
+                try:
+                    relationArticle = self.driver.find_element_by_class_name(
+                        "article_relation"
+                    ).text
 
-                articleText = articleText.replace(relationArticle, "")
-            except:
-                relationArticle = ""
+                    articleText = articleText.replace(relationArticle, "")
+                except:
+                    relationArticle = ""
 
-            print(title)
-            print("\n")
-            print(articleText)
-            print("\n")
-            for des in articlePhoto:
-                print(des.text, "\n")
-            print(relationArticle)
+                print(title)
+                print("\n")
+                print(articleText)
+                # print("\n")
+                # for des in articlePhoto:
+                #     print(des.text, "\n")
+                # print(relationArticle)
 
         except:
             print("ERROR")
+
         finally:
             self.driver.quit()
+            # self.driver.close()
 
     def getSite(self, catecory: str) -> None:
-        self.driver.get(self.catecory[catecory])
+        self.driver.get(self.catecoryDir[catecory])
         title: str = self.driver.find_element_by_xpath(
-            '//*[@id="content"]/div[3]/div[1]/div/div/ul/li[2]/div/a[2]'
+            '//*[@id="content"]/div[3]/div[1]/div/div/ul/li[1]/div/a[2]'
         )
         title.click()
         return
