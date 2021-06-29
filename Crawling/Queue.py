79ba0a55547d73c3599456372236465c2fdad38e
unknown <jo7hb@naver.com>
2021-06-29 23:41:03 +0900

    custom Queue

---
+++ Crawling/Queue.py
@@ -0,0 +1,39 @@
+class Queue:
+    def __init__(self, capacity: int) -> None:
+        self._queue = []
+        self._capacity = capacity
+
+    def size(self) -> int:
+        return len(self._queue)
+
+    def push_front(self, element: any) -> None:
+        temp: list = [element]
+        self._queue = temp + self._queue
+
+        if len(self._queue) > self._capacity:
+            self._queue = self._queue[0 : self._capacity]
+
+        return
+
+    def push_back(self, element: any) -> None:
+        self._queue.append(element)
+        if len(self._queue) > self._capacity:
+            self._queue = self._queue[
+                len(self._queue) - self._capacity : len(self._queue)
+            ]
+        return
+
+    def pop_back(self) -> any:
+        returnValue = self._queue.pop()
+        return returnValue
+
+    def pop_front(self) -> any:
+        returnValue = self._queue[0]
+        self._queue = self._queue[1 : len(self._queue)]
+        return returnValue
+
+    def delete(self, index) -> None:
+        self._queue = self._queue[0:index] + self._queue[index + 1 : len(self._queue)]
+
+    def refer(self, index: int) -> any:
+        return self._queue[index]
