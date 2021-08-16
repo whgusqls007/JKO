5068dde15efb9848fa10a4f0fd5c88dd951ce6c2
Jo Hyunbin <jo7hb@naver.com>
2021-08-16 23:33:51 +0900

    Test Local Storage

--- client-app/src/components/Card.js
+++ client-app/src/components/Card.js
@@ -1,193 +1,199 @@
 import React, { useState, useEffect, useRef } from "react";
 import {
-    View,
-    Text,
-    StyleSheet,
-    FlatList,
-    TouchableOpacity,
+	View,
+	Text,
+	StyleSheet,
+	FlatList,
+	TouchableOpacity,
 } from "react-native";
 import { Card } from "react-native-elements";
 import ArticleModal from "./ArticleModal";
 
 const CardComponent = (props) => {
-    const [data, setData] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [prevText, setPrevText] = useState("");
-    const [prevCategory, setPrevCategory] = useState("All");
-    const [firstParam, setFirstParam] = useState(10);
-    const [secondParam, setSecondParam] = useState(19);
-    const [fromFirst, setFromFirst] = useState(1);
+	const [data, setData] = useState([]);
+	const [loading, setLoading] = useState(true);
+	const [prevText, setPrevText] = useState("");
+	const [prevCategory, setPrevCategory] = useState("All");
+	const [firstParam, setFirstParam] = useState(10);
+	const [secondParam, setSecondParam] = useState(19);
+	const [fromFirst, setFromFirst] = useState(1);
 
-    const flatListRef = useRef();
+	const flatListRef = useRef();
 
-    const toTop = () => {
-        flatListRef.current.scrollToOffset({ animated: true, index: -1 });
-    };
+	const toTop = () => {
+		flatListRef.current.scrollToOffset({ animated: true, index: -1 });
+	};
 
-    if (fromFirst !== props.fromFirst) {
-        setFromFirst(props.fromFirst);
-    }
+	if (fromFirst !== props.fromFirst) {
+		setFromFirst(props.fromFirst);
+	}
 
-    if (prevText !== props.search) {
-        setPrevText(props.search);
-    }
+	if (prevText !== props.search) {
+		setPrevText(props.search);
+	}
 
-    if (prevCategory !== props.category) {
-        setPrevCategory(props.category);
-    }
+	if (prevCategory !== props.category) {
+		setPrevCategory(props.category);
+	}
 
-    const extraLoadData = () => {
-        fetch(props.pressURL, {
-            method: "POST",
-            headers: {
-                Accept: "application/json",
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                first: firstParam,
-                last: secondParam,
-                text: prevText,
-                category: prevCategory,
-            }),
-        })
-            .then((response) => response.json())
-            .then((loadedData) => {
-                let newData = data.concat(loadedData);
-                setData(newData);
-                setFirstParam(secondParam + 1);
-                setSecondParam(secondParam + 10);
-                setLoading(false);
-            })
-            .catch((e) => console.log(e));
-    };
+	const extraLoadData = () => {
+		fetch(props.pressURL, {
+			method: "POST",
+			headers: {
+				Accept: "application/json",
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				first: firstParam,
+				last: secondParam,
+				text: prevText,
+				category: prevCategory,
+			}),
+		})
+			.then((response) => response.json())
+			.then((loadedData) => {
+				let newData = data.concat(loadedData);
+				setData(newData);
+				setFirstParam(secondParam + 1);
+				setSecondParam(secondParam + 10);
+				setLoading(false);
+			})
+			.catch((e) => console.log(e));
+	};
 
-    const loadData = () => {
-        fetch(props.pressURL, {
-            method: "POST",
-            headers: {
-                Accept: "application/json",
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                first: 0,
-                last: 9,
-                text: props.search,
-                category: props.category,
-            }),
-        })
-            .then((response) => response.json())
-            .then((loadedData) => {
-                setData(loadedData);
-                setFirstParam(10);
-                setSecondParam(19);
-                setLoading(false);
-            })
-            .catch((e) => console.log(e));
-    };
+	const loadData = () => {
+		fetch(props.pressURL, {
+			method: "POST",
+			headers: {
+				Accept: "application/json",
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				first: 0,
+				last: 9,
+				text: props.search,
+				category: props.category,
+			}),
+		})
+			.then((response) => response.json())
+			.then((loadedData) => {
+				setData(loadedData);
+				setFirstParam(10);
+				setSecondParam(19);
+				setLoading(false);
+			})
+			.catch((e) => console.log(e));
+	};
 
-    const setTrue = (ID) => {
-        setData(
-            data.map((item) => {
-                if (item.id === ID) {
-                    return {
-                        id: item.id,
-                        title: item.title,
-                        mainText: item.mainText,
-                        visible: true,
-                    };
-                } else {
-                    return item;
-                }
-            })
-        );
-    };
+	const setTrue = (ID) => {
+		setData(
+			data.map((item) => {
+				if (item.id === ID) {
+					return {
+						id: item.id,
+						title: item.title,
+						mainText: item.mainText,
+						reporter: item.reporter,
+						date: item.date,
+						url: item.url,
+						visible: true,
+					};
+				} else {
+					return item;
+				}
+			})
+		);
+	};
 
-    const setFalse = (ID) => {
-        setData(
-            data.map((item) => {
-                if (item.id === ID) {
-                    return {
-                        id: item.id,
-                        title: item.title,
-                        mainText: item.mainText,
-                        visible: false,
-                    };
-                } else {
-                    return item;
-                }
-            })
-        );
-    };
+	const setFalse = (ID) => {
+		setData(
+			data.map((item) => {
+				if (item.id === ID) {
+					return {
+						id: item.id,
+						title: item.title,
+						mainText: item.mainText,
+						reporter: item.reporter,
+						date: item.date,
+						url: item.url,
+						visible: false,
+					};
+				} else {
+					return item;
+				}
+			})
+		);
+	};
 
-    useEffect(() => loadData(), []);
+	useEffect(() => loadData(), []);
 
-    useEffect(() => {
-        setLoading(true);
-        loadData();
-        toTop();
-    }, [prevText]);
+	useEffect(() => {
+		setLoading(true);
+		loadData();
+		toTop();
+	}, [prevText]);
 
-    useEffect(() => {
-        setLoading(true);
-        loadData();
-        toTop();
-    }, [prevCategory]);
+	useEffect(() => {
+		setLoading(true);
+		loadData();
+		toTop();
+	}, [prevCategory]);
 
-    useEffect(() => {
-        toTop();
-    }, [fromFirst]);
+	useEffect(() => {
+		toTop();
+	}, [fromFirst]);
 
-    const Item = ({ item }) => {
-        return (
-            <View>
-                <ArticleModal item={item} FsetFalse={setFalse} />
-                <TouchableOpacity onPress={() => setTrue(item.id)}>
-                    <Card>
-                        <Text style={styles.titleStyle2}>{item.title}</Text>
-                        <Text></Text>
-                        <Text numberOfLines={3}>{item.mainText}</Text>
-                    </Card>
-                </TouchableOpacity>
-            </View>
-        );
-    };
+	const Item = ({ item }) => {
+		return (
+			<View>
+				<ArticleModal item={item} FsetFalse={setFalse} />
+				<TouchableOpacity onPress={() => setTrue(item.id)}>
+					<Card>
+						<Text style={styles.titleStyle2}>{item.title}</Text>
+						<Text></Text>
+						<Text numberOfLines={3}>{item.mainText}</Text>
+					</Card>
+				</TouchableOpacity>
+			</View>
+		);
+	};
 
-    return (
-        <View style={styles.container}>
-            <FlatList
-                style={{ width: "100%" }}
-                data={data}
-                renderItem={(item) => {
-                    return Item(item);
-                }}
-                onRefresh={() => {
-                    loadData();
-                    setFirstParam(10);
-                    setSecondParam(20);
-                }}
-                refreshing={loading}
-                keyExtractor={(item) => item.id}
-                onEndReached={() => {
-                    setLoading(true);
-                    extraLoadData();
-                }}
-                onEndReachedThreshold={0.9}
-                ref={flatListRef}
-            />
-        </View>
-    );
+	return (
+		<View style={styles.container}>
+			<FlatList
+				style={{ width: "100%" }}
+				data={data}
+				renderItem={(item) => {
+					return Item(item);
+				}}
+				onRefresh={() => {
+					loadData();
+					setFirstParam(10);
+					setSecondParam(20);
+				}}
+				refreshing={loading}
+				keyExtractor={(item) => item.id}
+				onEndReached={() => {
+					setLoading(true);
+					extraLoadData();
+				}}
+				onEndReachedThreshold={0.9}
+				ref={flatListRef}
+			/>
+		</View>
+	);
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignContent: "center",
-        alignItems: "center",
-        width: "100%",
-    },
-    titleStyle2: {
-        fontSize: 20,
-    },
+	container: {
+		flex: 1,
+		alignContent: "center",
+		alignItems: "center",
+		width: "100%",
+	},
+	titleStyle2: {
+		fontSize: 20,
+	},
 });
 
 export default CardComponent;
--- client-app/src/components/ScreenHeader.js
+++ client-app/src/components/ScreenHeader.js
@@ -1,8 +1,55 @@
-import React from "react";
-import { View, StyleSheet, Text, Platform } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, StyleSheet, Text, Platform, CheckBox } from "react-native";
 import { AntDesign } from "@expo/vector-icons";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const ScreenHeader = (props) => {
+	const [isSelected, setSelection] = useState(false);
+
+	const storeSubScribe = async (value) => {
+		try {
+			const jsonValue = JSON.stringify(value);
+			await AsyncStorage.setItem(props.screenName, jsonValue);
+		} catch (e) {
+			console.log(e);
+		}
+	};
+
+	const getSubScribe = async () => {
+		try {
+			const jsonValue = await AsyncStorage.getItem(props.screenName);
+			return jsonValue != null ? JSON.parse(jsonValue) : null;
+		} catch (e) {
+			console.log(e);
+		}
+	};
+
+	useEffect(() => {
+		let value = getSubScribe();
+		if (value != null) {
+			if (value[props.screenName] === true) {
+				setSelection(true);
+			} else {
+				setSelection(false);
+			}
+		}
+	}, []);
+
+	useEffect(() => {
+		let value = getSubScribe();
+		if (value != null) {
+			if (isSelected === true) {
+				value[props.screenName] = true;
+			} else {
+				value[props.screenName] = false;
+			}
+		} else {
+			value[props.screenName] = false;
+		}
+		console.log(value);
+		storeSubScribe(value);
+	}, [isSelected]);
+
 	return (
 		<View style={styles.header}>
 			<AntDesign.Button
@@ -15,6 +62,14 @@ const ScreenHeader = (props) => {
 			<Text style={styles.text}>
 				{props.screenName} : {props.categoryLabelValue}
 			</Text>
+			<View style={styles.subScribe}>
+				<Text style={styles.subScribeText}>구독</Text>
+				<CheckBox
+					value={isSelected}
+					onValueChange={setSelection}
+					style={styles.subScribeBox}
+				/>
+			</View>
 			<View style={{ flexDirection: "row" }}>
 				<AntDesign.Button
 					name="search1"
@@ -53,6 +108,21 @@ if (Platform.OS === "android") {
 			textAlign: "center",
 			textAlignVertical: "center",
 			fontSize: 20,
+		},
+		subScribe: {
+			flexDirection: "row",
+		},
+		subScribeText: {
+			marginLeft: "5%",
+			textAlign: "center",
+			textAlignVertical: "center",
+			fontSize: 20,
+			marginRight: 0,
+			paddingRight: 0,
+		},
+		subScribeBox: {
+			marginLeft: 0,
+			paddingLeft: 0,
 		},
 	});
 } else {
