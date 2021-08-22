68b5dff9d46961a32b7df2e6ee75310526808d9b
Hyunbin Jo <jo7hb@naver.com>
2021-08-22 19:10:48 +0900

    Fix Joongang bug

--- Crawling/Joongang.py
+++ Crawling/Joongang.py
@@ -6,10 +6,10 @@ class Joongang(CrawlingDriver):
     def __init__(self):
         CrawlingDriver.__init__(self)
         self.url = {
-            "politics": "https://news.joins.com/politics?cloc=joongang-section-gnb2",
-            "economy": "https://news.joins.com/money?cloc=joongang-section-gnb3",
-            "society": "https://news.joins.com/society?cloc=joongang-section-gnb4",
-            "sport": "https://news.joins.com/sports?cloc=joongang-section-gnb7",
+            "politics": "https://www.joongang.co.kr/politics",
+            "economy": "https://www.joongang.co.kr/money",
+            "society": "https://www.joongang.co.kr/society",
+            "sport": "https://www.joongang.co.kr/sports",
         }
 
     def crawling(self):
@@ -19,14 +19,16 @@ class Joongang(CrawlingDriver):
                 self.getSite(self.url[category])
                 try:
                     article = self.getElement(
-                        f"//*[@id='content']/div[3]/ul/li[{i}]/h2/a"
+                        f'//*[@id="story_list"]/li[{i}]/div[2]/h2/a'
                     )
                 except:
                     continue
+
                 title = article.text
+
                 try:
                     date = self.getElement(
-                        f"//*[@id='content']/div[3]/ul/li[{i}]/span[3]"
+                        f'//*[@id="story_list"]/li[{i}]/div[2]/div/p'
                     ).text
                 except:
                     continue
@@ -35,33 +37,26 @@ class Joongang(CrawlingDriver):
 
                 time.sleep(2)
 
-                try:
-                    mainText = self.getElement("//*[@id='article_body']").text
-                except:
-                    continue
-
+                mainText = ''
                 j = 1
                 while True:
                     try:
-                        subText = self.getElement(
-                            "//*[@id='article_body']/div[{j}]"
-                        ).text
-                        mainText = mainText.replace(subText, "")
+                        mainText += self.getElement(f'//*[@id="article_body"]/p[{j}]').text
                         j += 1
                     except:
                         break
+
+
                 try:
                     reporter = self.getElement(
-                        "//*[@id='content']/div[1]/div/dl/dd/span/strong/a"
+                        '//*[@id="container"]/section/article/header/div[3]/a'
                     ).text
                 except:
                     continue
 
                 url = self.driver.current_url
-
-                mainText = mainText.replace("\n", " ")
-
-                date = date.replace(".", "-")
+                
+                date = date.replace('.', '-')
 
                 self.articles.append(
                     {
